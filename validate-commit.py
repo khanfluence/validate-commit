3bc9770b470a67285b3a393793a1b3a809edf2bc
Ryan Santos <ryan.santos@teradata.com>
2018-05-29 10:01:55 -0700

    Use subprocess to get commit message using git show

--- validate-commit.py
+++ validate-commit.py
@@ -1,8 +1,10 @@
 import re
+import subprocess
 
 
-def validate(message):
-    # print(message + "\n-----")
+def is_valid_message():
+    message = subprocess.run("git show -s --format=%B", shell=True, stdout=subprocess.PIPE, cwd="..\stacki")\
+        .stdout.decode("utf-8").strip()
 
     # merge (ignore)
     if message.startswith("Merge"):
