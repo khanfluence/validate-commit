9d3a7d9087ccc51bd7bd429a41855b451c6e155d
Ryan Santos <ryan.santos@teradata.com>
2018-05-30 10:17:00 -0700

    Switch to logging

--- validate-commit.py
+++ validate-commit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import logging
 import re
 import subprocess
 import sys
 
+logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 def is_valid_message(commit=""):
     """
@@ -22,42 +24,58 @@ def is_valid_message(commit=""):
                             stdout=subprocess.PIPE, cwd="..\stacki")
     message = result.stdout.strip()
 
-    # special pass cases
-    if message.startswith("Merge") or message.startswith("Update README.md"):
-        print("pass: ignore")  # expand, log
+    # merge commit
+    if message.startswith("Merge"):
+        logging.info("Merge commit\n")
         return True
+
+    # README update
+    if message.startswith("Update README.md"):
+        logging.info("README update\n")
+        return True
+
+    # jira in oneline
+    if re.search(r"^.*(STACKI-\d+|JIRA)", message):
+        logging.info("JIRA mention in oneline\n")
 
     # prefix
     if not re.search(r"^(BUGFIX|INTERNAL|FEATURE|DOCS):", message):
-        print("fail: prefix")  # expand, log
+        logging.info("No valid prefix\n")
         return False
 
-    # multi-line cases
     if '\n' in message:
-        # blank line; regex: exactly one blank line following oneline
+        # blank line; regex: exactly one blank line after oneline
         if not re.search(r"^.+\n\n.+", message):
-            print("fail: blank line")  # expand, log
+            logging.info("No single blank line after oneline\n")
             return False
 
-        # jira
+        # jira in body
         if re.search(r"STACKI-\d+", message) and "JIRA" not in message:
-            print("fail: jira")  # expand, log
+            logging.info("No JIRA mention in body when referencing ticket\n")
             return False
 
-    print("pass")  # expand, log
+    logging.info("No special case\n")
     return True
 
 
 def main():
-    if len(sys.argv) > 0 and sys.argv[1] == "test":
+    if len(sys.argv) == 1 or sys.argv[1] != "test":
+        if is_valid_message():
+            print("Pass")
+            sys.exit(0)
+        else:
+            print("Fail")
+            sys.exit(1)
+    else:
+        # test with recent commits
         result = subprocess.run("git log --format=format:%H",
                                 encoding="utf-8", shell=True,
                                 stdout=subprocess.PIPE, cwd="..\stacki")
-        for commit in result.stdout.strip().split()[:50]:
-            is_valid_message(commit)  # print commit message?
-            # line after if?
-    else:  # explicit else?
-        is_valid_message()
+        for commit_id in result.stdout.strip().split()[:20]:
+            logging.info(commit_id)
+            print("Pass" if is_valid_message(commit_id) else "Fail")
+
+        sys.exit(0)
 
 
 if __name__ == "__main__":
